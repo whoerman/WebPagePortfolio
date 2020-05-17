899ebc0271d85fab479c189e7aa34f94eb0e469a
Walter Hoerman <whoerman@gmail.com>
2020-05-17 16:54:17 -0400

    starting the react app

---
+++ webpageportfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from "./components/pages/Home";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpageportfolio/src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Home() {
+  return (
+    <div className = "HomeDiv">
+      <h1 className="text-center">Hello, I'm Walter Hoerman, MD</h1>
+      <h1 className="text-center">Peditrician, Politician, Web Developer.</h1>
+    </div>
+  );
+}
+
+export default Home;
